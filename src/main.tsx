d0241697450f442470062419d15bb5c7a6e9c74e
sydbarrettrno <sydbarrettrno@gmail.com>
2026-09-01 08:25:40 -0300

    Carrega estilos do filtro por categorias

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./indicator-enhancements.css";
 import "./flow-audit.css";
 import "./executive-reference-v2.css";
 import "./ux-security-v1.css";
+import "./category-filter.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
